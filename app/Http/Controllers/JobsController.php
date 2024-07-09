7e62080ca6bdb10cf424c2dcedaa2d8bd4ccb684
Muntazir0972 <mohammadmuntazir18@gmail.com>
2024-07-09 13:05:32 +0500

    apply filters,14

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -17,12 +17,59 @@ use Intervention\Image\Drivers\Gd\Driver;
 
 class JobsController extends Controller
 {
-    public function index(){
+    public function index(Request $data){
 
         $categories = Category::where('status',1)->get();
         $jobTypes = Job_Type::where('status',1)->get();
-        $jobs = Job::where('status',1)->with('jobType')->orderby('created_at','DESC')->paginate(9);
 
-        return view('front.jobs',compact('categories','jobTypes','jobs'));
+        $jobs = Job::where('status',1);
+
+        //search using keyword
+        if (!empty($data->keyword)) {
+            
+            $jobs = $jobs->where(function($query) use ($data){
+                $query->orWhere('title','like','%'.$data->keyword.'%');
+                $query->orWhere('keywords','like','%'.$data->keyword.'%');
+                
+            });
+        }
+
+        //search using location
+        if (!empty($data->location)) {
+            $jobs = $jobs->where('location',$data->location);
+        }
+
+        //search using location
+        if (!empty($data->category)) {
+                $jobs = $jobs->where('category_id',$data->category);
+        }
+
+        $jobTypeArray =[];
+        //search using job type
+        if (!empty($data->jobType)) {
+            //1,2,3
+            $jobTypeArray = explode(',',$data->jobType);
+            $jobs = $jobs->whereIn('job_type_id',$jobTypeArray);
+        }
+     
+        //search using experience
+        if (!empty($data->experience)) {
+            $jobs = $jobs->where('experience',$data->experience);
+        }
+
+
+        $jobs = $jobs->with(['jobType','category']);
+
+        if ($data->sort == '0') {
+
+            $jobs = $jobs->orderBy('created_at','ASC');
+            
+        }else{
+            $jobs = $jobs->orderBy('created_at','DESC');
+        }
+
+        $jobs = $jobs ->paginate(9);
+
+        return view('front.jobs',compact('categories','jobTypes','jobs','jobTypeArray'));
     }
 }
