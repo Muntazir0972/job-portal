344678d4bc028bf089038a3fad679d1b0be2863e
Muntazir0972 <mohammadmuntazir18@gmail.com>
2024-07-18 13:39:59 +0500

    delete job applications,34

---
+++ app/Http/Controllers/admin/JobApplicationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\Job_Type;
+use App\Models\JobApplication;
+use Illuminate\Support\Facades\Validator;
+use App\Models\SavedJobs;
+use Illuminate\Routing\Route;
+use PhpParser\Node\Stmt\Echo_;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class JobApplicationController extends Controller
+{
+    public function index(){
+
+        $applications = JobApplication::orderBy('created_at','ASC')
+                                        ->with('job','user','employer')
+                                        ->paginate(10);
+
+        return view('admin.job-applications.list',compact('applications'));
+    }   
+}
